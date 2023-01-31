0dc888ab61dd5485a0f6f889f99b06079be885f6
Maciej <maciejwalega17@gmail.com>
2023-01-31 14:24:07 +0100

    needs to be slow cus js and mms

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 require('chromedriver');
 
-const data = ['5091882', '060', '080'];
-const mc = 3;
+const data = ['100001', '040', '060'];
+const mc = 1;
 
 const detal = data.slice(0, 1);
 const operations = data.slice(1);
@@ -21,375 +21,205 @@ for (const op of operations) {
 }
 
 const test = async () => {
-	// Nawiguj do kaatalogu z mocowaniami
+	// Stwórz driver
 	let driver = await new Builder().forBrowser('chrome').build();
+
+	// Funkcje
+	const xpathClick = async (xp) => {
+		await driver.wait(until.elementLocated(By.xpath(xp))).click();
+	};
+	const xpathKeys = async (xp, k) => {
+		await driver.wait(until.elementLocated(By.xpath(xp))).sendKeys(k);
+	};
+	const idClick = async (i) => {
+		await driver.wait(until.elementLocated(By.id(i))).click();
+	};
+	const idKeys = async (i, k) => {
+		await driver.wait(until.elementLocated(By.id(i))).sendKeys(k);
+	};
+	const selectSearchFilter = async (q) => {
+		idKeys('selection-search-filter', q);
+		await driver.sleep(700);
+		idKeys('selection-search-filter', Key.RETURN);
+		// xpathClick(
+		// 	'/html/body/div/div[4]/div/mat-dialog-container/dialog-select-nc-program/mat-dialog-actions/mms-btn[1]'
+		// );
+	};
+
+	// Wejdz na strone mms i zaloguj
 	await driver.get('http://mms251107s1/#/');
-	await driver
-		.wait(until.elementLocated(By.xpath('//*[@id="en-US"]/img')), 2000)
-		.click();
-	await driver.wait(until.elementLocated(By.xpath('//a[1]')), 5000).click();
-	await driver
-		.wait(until.elementLocated(By.id('username')), 2000)
-		.sendKeys('Ur');
-	await driver
-		.wait(until.elementLocated(By.id('password')), 2000)
-		.sendKeys('password', Key.RETURN);
-	await driver.wait(until.elementLocated(By.id('m1_masterdata')), 2000).click();
-
-	//Dodaj fixtures master data
+	xpathClick(
+		'/html/body/mms-app-root/mms-language-selection/div[2]/div/div/a[3]'
+	);
+	xpathClick(
+		'/html/body/mms-app-root/mms-home/div[2]/mms-menu/div/div[1]/a[1]'
+	);
+	idKeys('username', 'Ur');
+	idKeys('password', 'password');
+	await driver.sleep(2000);
+	idKeys('password', Key.RETURN);
+	// Idź do fixtures master data
+	idClick('m1_masterdata');
+	// Pętla dodawania fixtures
 	for (let i = 0; i < fixtureList.length; i++) {
-		await driver.sleep(400);
-		await driver
-			.wait(until.elementLocated(By.id('action_add_fixture')), 2000)
-			.click();
-		await driver
-			.wait(
-				until.elementLocated(
-					By.xpath('//fieldset[1]/mms-input-group/div/input')
-				)
-			)
-			.sendKeys(fixtureList[i]);
+		xpathClick(
+			'/html/body/mms-app-root/datamanager-app/datamanager-fixtures/manager-page-header/div[2]/a[1]'
+		);
 		await driver.sleep(500);
-		await driver
-			.wait(
-				until.elementLocated(
-					By.xpath('//fieldset[2]/mms-input-group/div/input')
-				)
-			)
-			.sendKeys(`Setup ${i + 1}`);
-		await driver
-			.wait(until.elementLocated(By.id('edit_fixture_save')), 2000)
-			.click();
-	}
-
-	// Nawiguj do katalogu part master data
-	await driver
-		.wait(until.elementLocated(By.id('m2_part-master-data')), 2000)
-		.click();
-	await driver.wait(until.elementLocated(By.id('action_add')), 2000).click();
-
-	// Dodaj part master data
-	await driver
-		.findElement(By.xpath('//fieldset[1]/mms-input-group[1]/div/input'))
-		.sendKeys(detal[0], Key.RETURN);
-
-	await driver
-		.wait(
-			until.elementLocated(
-				By.xpath(
-					'/html/body/div/div[4]/div/mat-dialog-container/dialog-add-new-operation/mat-dialog-content/form/fieldset[1]/mms-input-group/div/input'
-				)
-			),
-			2000
-		)
-		.sendKeys('400');
-	await driver
-		.wait(
-			until.elementLocated(
-				By.xpath(
-					'/html/body/div/div[4]/div/mat-dialog-container/dialog-add-new-operation/mat-dialog-actions/mms-btn[1]'
-				)
-			),
-			2000
-		)
-		.click();
-
-	// Dodaj operacje
-	for (let i = 0; i < operations.length; i++) {
+		xpathKeys(
+			'/html/body/div/div[2]/div/mat-dialog-container/dialog-edit-fixture/mat-dialog-content/form/fieldset[1]/mms-input-group/div/input',
+			fixtureList[i]
+		);
+		await driver.sleep(700);
+		xpathKeys(
+			'/html/body/div/div[2]/div/mat-dialog-container/dialog-edit-fixture/mat-dialog-content/form/fieldset[2]/mms-input-group/div/input',
+			`Setup ${i + 1}`
+		);
 		await driver.sleep(1000);
-		await driver
-			.wait(
-				until.elementLocated(By.xpath("//span[text()='Add operation']")),
-				2000
-			)
-			.click();
-
-		await driver
-			.wait(
-				until.elementLocated(
-					By.xpath(
-						'/html/body/div/div[2]/div/mat-dialog-container/dialog-add-new-operation/mat-dialog-content/form/fieldset[1]/mms-input-group/div/input'
-					)
-				),
-				2000
-			)
-			.sendKeys(operations[i]);
+		xpathClick(
+			'/html/body/div/div[2]/div/mat-dialog-container/dialog-edit-fixture/mat-dialog-actions/mms-btn[1]'
+		);
+	}
+	// Nawiguj do katalogu part master data
+	await driver.sleep(1500);
+	idClick('m2_part-master-data');
+	// Dodaj part master data
+	idClick('action_add');
+	await driver.sleep(1000);
+	xpathKeys(
+		'/html/body/div/div[2]/div/mat-dialog-container/dialog-add-new-item/mat-dialog-content/form/fieldset[1]/mms-input-group[1]/div/input',
+		detal[0]
+	);
+	xpathKeys(
+		'/html/body/div/div[2]/div/mat-dialog-container/dialog-add-new-item/mat-dialog-content/form/fieldset[1]/mms-input-group[1]/div/input',
+		Key.RETURN
+	);
+	// Uzupełnij pierwszą operację (usuwana na koniec)
+	xpathKeys(
+		'/html/body/div/div[4]/div/mat-dialog-container/dialog-add-new-operation/mat-dialog-content/form/fieldset[1]/mms-input-group/div/input',
+		'400'
+	);
+	xpathClick(
+		'/html/body/div/div[4]/div/mat-dialog-container/dialog-add-new-operation/mat-dialog-actions/mms-btn[1]'
+	);
+	await driver.sleep(500);
+	// Pętla dodania operacji i uzupełnienia kroków
+	for (let i = 0; i < operations.length; i++) {
+		// Dodaj operacje
+		await driver.sleep(1500);
+		xpathClick("//span[text()='Add operation']");
+		xpathKeys(
+			'/html/body/div/div[2]/div/mat-dialog-container/dialog-add-new-operation/mat-dialog-content/form/fieldset[1]/mms-input-group/div/input',
+			operations[i]
+		);
 		await driver.sleep(500);
-		await driver
-			.wait(
-				until.elementLocated(
-					By.xpath(
-						'/html/body/div/div[2]/div/mat-dialog-container/dialog-add-new-operation/mat-dialog-content/form/fieldset[3]/mms-input-group/div/input'
-					)
-				),
-				2000
-			)
-			.sendKeys(`Setup ${i + 1}`, Key.RETURN);
-
+		xpathKeys(
+			'/html/body/div/div[2]/div/mat-dialog-container/dialog-add-new-operation/mat-dialog-content/form/fieldset[3]/mms-input-group/div/input',
+			`Setup ${i + 1}`
+		);
+		await driver.sleep(500);
+		xpathKeys(
+			'/html/body/div/div[2]/div/mat-dialog-container/dialog-add-new-operation/mat-dialog-content/form/fieldset[3]/mms-input-group/div/input',
+			Key.RETURN
+		);
 		// Dodaj kroki do operacji i wypełnij je danymi
-		await driver.sleep(500);
-		// Dodaj kroki operacji
-		await driver
-			.wait(
-				until.elementLocated(
-					By.xpath(
-						`/html/body/mms-app-root/datamanager-app/datamanager-part-master-data/manager-page-content/manager-page-detail/datamanager-part-details/div/div[${
-							i + 2
-						}]/div/div[1]/mms-btn[2]`
-					)
-				),
-				2000
-			)
-			.click();
-
+		// Dodaj krok operacji
+		await driver.sleep(1000);
+		xpathClick(
+			`/html/body/mms-app-root/datamanager-app/datamanager-part-master-data/manager-page-content/manager-page-detail/datamanager-part-details/div/div[${
+				i + 2
+			}]/div/div[1]/mms-btn[2]`
+		);
 		// Dodaj Fixture
-		await driver
-			.wait(
-				until.elementLocated(
-					By.xpath(
-						'/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[1]/mms-input-group[2]/div/mms-btn'
-					)
-				),
-				2000
-			)
-			.click();
-		await driver
-			.wait(until.elementLocated(By.id('selection-search-filter')), 2000)
-			.sendKeys(fixtureList[i]);
-		await driver
-			.wait(until.elementLocated(By.xpath("//span[text()='Select']")), 2000)
-			.click();
+		await driver.sleep(1000);
+		xpathClick(
+			'/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[1]/mms-input-group[2]/div/mms-btn'
+		);
+		selectSearchFilter(fixtureList[i]);
+		await driver.sleep(1000);
 		// Loading
-		await driver
-			.wait(
-				until.elementLocated(
-					By.xpath(
-						'/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[2]/div[1]/datamanager-edit-loading-step/div/div[2]/input'
-					)
-				),
-				2000
-			)
-			.click();
-		await driver
-			.wait(
-				until.elementLocated(
-					By.xpath(
-						'/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[2]/div[1]/datamanager-edit-loading-step/div/mms-input-group/div/mms-input-time/input'
-					)
-				),
-				2000
-			)
-			.sendKeys('5:00');
+		xpathClick(
+			'/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[2]/div[1]/datamanager-edit-loading-step/div/div[2]/input'
+		);
+		xpathKeys(
+			'/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[2]/div[1]/datamanager-edit-loading-step/div/mms-input-group/div/mms-input-time/input',
+			'5:00'
+		);
+		await driver.sleep(1000);
 		// Unloading
-		await driver.sleep(500);
-		await driver
-			.wait(until.elementLocated(By.xpath('//li[3]/span')), 2000)
-			.click();
-
-		await driver
-			.wait(
-				until.elementLocated(
-					By.xpath(
-						'/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[2]/div[3]/datamanager-edit-unloading-step/div/div[2]/input'
-					)
-				),
-				2000
-			)
-			.click();
-
-		await driver
-			.wait(
-				until.elementLocated(
-					By.xpath(
-						'/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[2]/div[3]/datamanager-edit-unloading-step/div/mms-input-group/div/mms-input-time/input'
-					)
-				),
-				2000
-			)
-			.sendKeys('5:00');
-		// Dodaj kroki Machining
+		xpathClick('//li[3]/span');
+		xpathClick(
+			'/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[2]/div[3]/datamanager-edit-unloading-step/div/div[2]/input'
+		);
+		xpathKeys(
+			'/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[2]/div[3]/datamanager-edit-unloading-step/div/mms-input-group/div/mms-input-time/input',
+			'5:00'
+		);
+		await driver.sleep(1000);
+		// Pętla dodaj kroki Machining i zaznacz maszynę
 		for (let i = 0; i < 3; i++) {
-			await driver.sleep(500);
-
-			await driver
-				.wait(until.elementLocated(By.xpath(`//li[${2 + i}]/span`)), 2000)
-				.click();
-			await driver
-				.wait(
-					until.elementLocated(
-						By.xpath(
-							`/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[2]/div[${
-								2 + i
-							}]/datamanager-edit-machining-step/div/div[2]/div[2]/div[1]/div[${mc}]/input`
-						)
-					),
-					2000
-				)
-				.click();
-			await driver
-				.wait(
-					until.elementLocated(
-						By.xpath(
-							'/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[2]/nav/ul/div/mms-btn[2]'
-						)
-					),
-					2000
-				)
-				.click();
-			await driver
-				.wait(
-					until.elementLocated(
-						By.xpath(
-							'/html/body/div/div[4]/div/mat-dialog-container/dialog-select-step-type/mat-dialog-content/div/button[1]'
-						)
-					),
-					2000
-				)
-				.click();
+			await driver.sleep(1000);
+			xpathClick(`//li[${2 + i}]/span`);
+			xpathClick(
+				`/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[2]/div[${
+					2 + i
+				}]/datamanager-edit-machining-step/div/div[2]/div[2]/div[1]/div[${mc}]/input`
+			);
+			xpathClick(
+				'/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[2]/nav/ul/div/mms-btn[2]'
+			);
+			xpathClick(
+				'/html/body/div/div[4]/div/mat-dialog-container/dialog-select-step-type/mat-dialog-content/div/button[1]'
+			);
 		}
-
+		await driver.sleep(700);
 		// Dodanie ostatniego MC poza petla
-		await driver.sleep(400);
-		await driver
-			.wait(until.elementLocated(By.xpath(`//li[${5}]/span`)), 2000)
-			.click();
-		await driver
-			.wait(
-				until.elementLocated(
-					By.xpath(
-						`/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[2]/div[${5}]/datamanager-edit-machining-step/div/div[2]/div[2]/div[1]/div[${mc}]/input`
-					)
-				),
-				2000
-			)
-			.click();
-		// Dodanie zerowan
-		await driver
-			.wait(until.elementLocated(By.xpath(`//li[${2}]/span`)), 2000)
-			.click();
-		await driver
-			.wait(
-				until.elementLocated(
-					By.xpath(
-						`/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[2]/div[${2}]/datamanager-edit-machining-step/div/div[2]/div[2]/div[3]/mms-btn`
-					)
-				),
-				2000
-			)
-			.click();
-		//funkcja selection search filter
-		await driver
-			.wait(until.elementLocated(By.id('selection-search-filter')), 2000)
-			.sendKeys('zerowanie');
-		await driver.sleep(500);
-		await driver
-			.wait(until.elementLocated(By.xpath("//span[text()='Select']")), 2000)
-			.click();
-
-		await driver
-			.wait(until.elementLocated(By.xpath(`//li[${4}]/span`)), 2000)
-			.click();
-		await driver
-			.wait(
-				until.elementLocated(
-					By.xpath(
-						`/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[2]/div[${4}]/datamanager-edit-machining-step/div/div[2]/div[2]/div[3]/mms-btn`
-					)
-				),
-				2000
-			)
-			.click();
-
-		await driver
-			.wait(until.elementLocated(By.id('selection-search-filter')), 2000)
-			.sendKeys('zerowanie');
-		await driver.sleep(500);
-		await driver
-			.wait(until.elementLocated(By.xpath("//span[text()='Select']")), 2000)
-			.click();
-
-		// Cleaning program
-
-		await driver
-			.wait(until.elementLocated(By.xpath(`//li[${5}]/span`)), 2000)
-			.click();
-		await driver
-			.wait(
-				until.elementLocated(
-					By.xpath(
-						`/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[2]/div[${5}]/datamanager-edit-machining-step/div/div[2]/div[2]/div[3]/mms-btn`
-					)
-				),
-				2000
-			)
-			.click();
-		//funkcja selection search filter
-		await driver
-			.wait(until.elementLocated(By.id('selection-search-filter')), 2000)
-			.sendKeys('cleaning');
-		await driver.sleep(500);
-		await driver
-			.wait(until.elementLocated(By.xpath("//span[text()='Select']")), 2000)
-			.click();
-
-		//nc program dodanie
-		await driver
-			.wait(until.elementLocated(By.xpath(`//li[${3}]/span`)), 2000)
-			.click();
-		await driver
-			.wait(
-				until.elementLocated(
-					By.xpath(
-						`/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[2]/div[${3}]/datamanager-edit-machining-step/div/div[2]/div[2]/div[3]/mms-btn`
-					)
-				),
-				2000
-			)
-			.click();
-		//funkcja selection search filter
-		await driver
-			.wait(until.elementLocated(By.id('selection-search-filter')), 2000)
-			.sendKeys(detal[0] + operations[i]);
-		await driver.sleep(500);
-		await driver
-			.wait(until.elementLocated(By.xpath("//span[text()='Select']")), 2000)
-			.click();
-
-		await driver
-			.wait(
-				until.elementLocated(
-					By.xpath(
-						`/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-actions/mms-btn[3]`
-					)
-				),
-				2000
-			)
-			.click();
+		xpathClick(`//li[${5}]/span`);
+		xpathClick(
+			`/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[2]/div[${5}]/datamanager-edit-machining-step/div/div[2]/div[2]/div[1]/div[${mc}]/input`
+		);
+		// DODWANIE PRAGRAMÓW ZEROWANIA
+		// Wybierz krok, kliknij dodaj program, wyszukaj i dodaj program x2
+		xpathClick(`//li[${2}]/span`);
+		xpathClick(
+			`/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[2]/div[${2}]/datamanager-edit-machining-step/div/div[2]/div[2]/div[3]/mms-btn`
+		);
+		selectSearchFilter('zerowanie');
+		await driver.sleep(1000);
+		xpathClick(`//li[${4}]/span`);
+		xpathClick(
+			`/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[2]/div[${4}]/datamanager-edit-machining-step/div/div[2]/div[2]/div[3]/mms-btn`
+		);
+		selectSearchFilter('zerowanie');
+		await driver.sleep(1000);
+		// DODWANIE PROGRAMU CZYSZCZENIA
+		xpathClick(`//li[${5}]/span`);
+		xpathClick(
+			`/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[2]/div[${5}]/datamanager-edit-machining-step/div/div[2]/div[2]/div[3]/mms-btn`
+		);
+		selectSearchFilter('cleaning');
+		await driver.sleep(1000);
+		// DODWANIE PROGRAMU NC
+		xpathClick(`//li[${3}]/span`);
+		xpathClick(
+			`/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-content/form[1]/div[2]/div/datamanager-edit-fms-process-plan/fieldset[2]/div[${3}]/datamanager-edit-machining-step/div/div[2]/div[2]/div[3]/mms-btn`
+		);
+		await driver.sleep(1000);
+		selectSearchFilter(detal[0] + operations[i]);
+		// Zapisz kroki do operacji
+		xpathClick(
+			`/html/body/div/div[2]/div/mat-dialog-container/dialog-process-plan-edit/mat-dialog-actions/mms-btn[3]`
+		);
 	}
 
-	//Usuwanie op10
-	await driver.sleep(500);
-	await driver
-		.wait(
-			until.elementLocated(
-				By.xpath(
-					`//*[@id="page-content"]/manager-page-detail/datamanager-part-details/div/div[${
-						operations.length + 2
-					}]/div/div[1]/mms-btn[1]`
-				)
-			),
-			2000
-		)
-		.click();
-	await driver
-		.wait(
-			until.elementLocated(
-				By.xpath(
-					'/html/body/mms-app-root/mms-alerts-overlay/div/div/mms-confirm/div[2]/mms-btn[1]'
-				)
-			),
-			2000
-		)
-		.click();
+	// Usuwanie operacji dodanej jako pierwsza
+	xpathClick(
+		`//*[@id="page-content"]/manager-page-detail/datamanager-part-details/div/div[${
+			operations.length + 2
+		}]/div/div[1]/mms-btn[1]`
+	);
+	xpathClick(
+		'/html/body/mms-app-root/mms-alerts-overlay/div/div/mms-confirm/div[2]/mms-btn[1]'
+	);
 };
 test();
